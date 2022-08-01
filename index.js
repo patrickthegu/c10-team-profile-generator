06db81aa3e58ed3bbf0c1c5a82220c56d1fdb01f
Patrick Gu <patrickthegu@gmail.com>
2022-08-01 18:50:34 +1000

    functions to take user input

--- index.js
+++ index.js
@@ -1,54 +1,259 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
-const questions = [
+// Array to store information for the whole team
+const team = [];
+
+// Arrays to store questions for each type of employee
+const managerQuestions = [
     {
         type: 'input',
-        name: 'manager',
-        message: 'Who is your team manager?'
+        name: 'name',
+        message: 'What is team manager\'s name?',
+        validate: answer => {
+            if (answer === ''){
+                return "Enter a valid name"
+            }else{
+                return true;
+            }
+        }
     },
     {
         type: 'input',
-        name: 'employeeid',
-        message: 'What is your employee id?'
+        name: 'id',
+        message: 'What is team manager\'s employee id?',
+        validate: answer => {
+            if (answer === ''){
+                return "Enter a valid manager employee id"
+            }else{
+                return true;
+            }
+        }
     },
     {
         type: 'input',
         name: 'email',
-        message: 'What is your email address?'
+        message: 'What is team manager\'s email address?',
+        validate: answer => {
+            if (answer === ''){
+                return "Enter a valid email address"
+            }else{
+                return true;
+            }
+        }
     },
     {
         type: 'input',
-        name: 'office',
-        message: 'What is your office number?'
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'What is your role?',
-        choices: [
-            'Engineer',
-            'Intern',
-            'Finish building my team'
-        ]  
+        name: 'officeNumber',
+        message: 'What is team manager\'s office number?',
+        validate: answer => {
+            if (answer === ''){
+                return "Enter a valid office number"
+            }else{
+                return true;
+            }
+        }
     }
 ];
 
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for my team members and their information
-// THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-// WHEN I click on an email address in the HTML
-// THEN my default email program opens and populates the TO field of the email with the address
-// WHEN I click on the GitHub username
-// THEN that GitHub profile opens in a new tab
-// WHEN I start the application
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your engineer\'s name?',
+        validate: answer => {
+            if (answer === ''){
+                return "Enter a valid engineer name"
+            }else{
+                return true;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your engineer\'s employee id?',
+        validate: answer => {
+            if (answer === ''){
+                return "Enter a valid engineer employee id"
+            }else{
+                return true;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your engineer\'s email address?',
+        validate: answer => {
+            if (answer === ''){
+                return "Enter a valid engineer email address"
+            }else{
+                return true;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your engineer\'s github username?',
+        validate: answer => {
+            if (answer === ''){
+                return "Enter a valid github username"
+            }else{
+                return true;
+            }
+        }
+    }
+];
+
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your intern\'s name?',
+        validate: answer => {
+            if (answer === ''){
+                return "Enter a valid intern name"
+            }else{
+                return true;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is your intern\'s employee id?',
+        validate: answer => {
+            if (answer === ''){
+                return "Enter a valid intern employee id"
+            }else{
+                return true;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your intern\'s email address?',
+        validate: answer => {
+            if (answer === ''){
+                return "Enter a valid intern email address"
+            }else{
+                return true;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'school',
+        message: 'What is your intern\'s school?',
+        validate: answer => {
+            if (answer === ''){
+                return "Enter a valid school"
+            }else{
+                return true;
+            }
+        }
+    }
+];
+
+const employeeQuestions = [
+    {
+        type: 'confirm',
+        name: 'addEmployee',
+        message: 'Do you want to add an employee?'        
+    },
+    {
+        
+        type: 'list',
+        name: 'typeEmployee',
+        message: 'What type of employee do you want to add?',
+        choices: [
+            'Engineer',
+            'Intern'
+        ]
+    }
+];
+
+// functions to add employee data to team array
+async function addManager(){
+    let managerResponse = await inquirer.prompt(managerQuestions);
+
+    let managerProfile = new Manager(managerResponse.name, managerResponse.id, managerResponse.email, managerResponse.officeNumber);
+
+    team.push(managerProfile);
+
+    console.log(`${managerProfile.name} has been added to the team`);
+};
+
+async function addEngineer(){
+    let engineerResponse = await inquirer.prompt(engineerQuestions);
+
+    let engineerProfile = new Engineer(engineerResponse.name, engineerResponse.id, engineerResponse.email, engineerResponse.github);
+    
+    team.push(engineerProfile);
+
+    console.log(`${engineerProfile.name} has been added to the team`);
+};
+
+async function addIntern(){
+    let internResponse = await inquirer.prompt(internQuestions);
+
+    let internProfile = new Intern(internResponse.name, internResponse.id, internResponse.email, internResponse.school);
+
+    team.push(internProfile);
+
+    console.log(`${managerProfile.name} has been added to the team`);
+};
+
+async function addEmployee(){
+    let confirmAdd = {
+            type: 'confirm',
+            name: 'addEmployee',
+            message: 'Do you want to add an employee?'        
+        };
+    
+    let type = {
+        type: 'list',
+        name: 'typeEmployee',
+        message: 'What type of employee do you want to add?',
+        choices: [
+            'Engineer',
+            'Intern'
+        ]
+    };
+
+    let employeeResponse = await inquirer.prompt(confirmAdd);
+
+    if (employeeResponse.addEmployee === true){
+        let employeeType = await inquirer.prompt(type);
+        switch (employeeType.typeEmployee){
+            case 'Engineer':
+                await addEngineer();
+                await addEmployee();
+                break;
+            case 'Intern':
+                await addIntern();
+                await addEmployee();
+                break;
+        }
+    }else {
+        return;
+    }
+}
+
+// Function to initialize app
+async function init() {
+    await addManager();
+    await addEmployee();
+    // fs.writeFile('dist/index.html', generateHTML(team));
+}
+
+// Function call to initialize app
+init();
